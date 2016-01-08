b1933561f369984c00e89a8e1d2a6a06b65ac1bf
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2016-01-08 11:30:21 +0100

    Updated pfw/prepare, now not explaoding in memory while still efficient

---
+++ miniproject/shootings.R
@@ -0,0 +1,27 @@
+## solution for miniproject shootings
+read.shootings <- function(file="counted.csv") {
+    x <- read.csv(file)
+    x
+}
+
+## painfull function, data is weirdly encoded
+read.votes <- function(file="gevotesbyparty.csv") {
+    x <- read.csv(file, skip=3, nrows=56)
+    names(x)[1] <- "state" ## for merge later
+    ## fix weird "," as 1000-separator in number strings
+    for (i in 2:4) {
+        x[,i] <- as.numeric(gsub(",", "", x[,i]))
+    }
+    x
+}
+
+shootings <- read.shootings()
+## count shootings and convert to dataframe
+## nshootings <- as.data.frame(table(state=shootings$state), responseName="nshootings")
+nshootings <- data.frame(table(shootings$state))
+names(nshootings) <- c("state", "nshootings")
+votes <- read.votes()
+
+## database join
+
+m <- merge(nshootings, votes, by="state")
--- pfw/prepare.py
+++ pfw/prepare.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-import gzip, csv, re, os
+## Read the wikipedia person.csv database, and split by birth year (if found)
+## Reading everything into memory, first, for speed efficiency reasons, does not work.
+## Keeping open files for every year, does not work, as there are too many open files.
+## Therefore we flush the file for a year every 1000 lines or so
 
-yearre = re.compile("(\d{4})-")
+import gzip, csv, re, os, sys
+
+yearre = re.compile("(-?\d{4})-")
 
 dest = "pfw/years"
 
+years = dict()
 with gzip.open("pfw/raw/Person.csv.gz") as fd:
     reader = csv.reader(fd)
     for i, row in enumerate(reader):
@@ -21,8 +27,24 @@ with gzip.open("pfw/raw/Person.csv.gz") as fd:
         m = yearre.search(date)
         if m:
             year = m.groups(0)[0]
-            f = os.path.join(dest, year)
-            with open(f, "a") as out:
-                writer = csv.writer(out, quoting=csv.QUOTE_NONNUMERIC)
-                writer.writerow(row)
-            print name, year
+            yearlist = years.get(year)
+            if not yearlist:
+                yearlist = []
+                years[year] = yearlist
+            yearlist.append(row)
+            if len(yearlist)>1000:
+                with open(os.path.join(dest, year), "a") as out:
+                    writer = csv.writer(out, quoting=csv.QUOTE_NONNUMERIC)
+                    writer.writerows(yearlist)
+                years[year] = []
+                print name, year
+
+sys.stdout.write("Writing remainders to disc")
+for year in years:
+    f = os.path.join(dest, year)
+    with open(f, "a") as out:
+        writer = csv.writer(out, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerows(years[year])
+        sys.stdout.write(".")
+        sys.stdout.flush()
+print "done"
